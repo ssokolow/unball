9eb58c2520fc43cd25a6943675257368646a7185
ssokolow <>
2006-12-22 23:48:47 -0400

    - Updated the message about tests which will always fail in install.sh

--- trunk/install.sh
+++ trunk/install.sh
@@ -101,7 +101,7 @@ if [ -w "$DESTDIR/$PREFIX" ] && [ "$1" != "--help" ] ; then
 	echo 
 	echo "unball installed. You can now type \"./run_test.py\" to run the unit tests. If tests fail, you probably are missing some extraction tools."
         echo
-	echo "Note: In this version of unball, there are six tests which always fail: Three for ziptest_bz2.zip and three for ziptest_ppmd.zip."
+	echo "Note: In this version of unball, there are three tests which always fail, all for ziptest_ppmd.zip."
 else
 	[ "$1" != "--help" ] && echo "Sorry, it appears that you do not have write permissions for the chosen install location."
 	echo "The install location can be adjusted using the DESTDIR and PREFIX environment variables."
