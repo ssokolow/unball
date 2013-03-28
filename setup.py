df9586485d86105d1c76dfed934f27d1b228593a
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2013-03-28 06:07:58 -0400

    Set up for tox and add an experimental setup.py

---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == '__main__' and not 'flake8' in sys.modules:
+    #FIXME: Why does this segfault flake8 under PyPy?
+    from setuptools import setup
+    import unball.main as unball
+
+    setup(
+        name="Unball",
+        version=unball.__version__,
+        description="'Do what I mean' archive commands for your shell",
+        long_description="""
+            Simple console wrappers which handle decisions like
+            "what format is it in?" and "Do I need to create a folder for it?"
+            for you.
+            """,  # TODO: Rewrite this when I finish making this an API with
+                  # console reference implementations.
+        author="Stephan Sokolow",
+        author_email="http://www.ssokolow.com/ContactMe",  # No spam harvesting
+        url='https://github.com/ssokolow/unball',
+        #download_url="https://github.com/ssokolow/unball",
+        license="License :: OSI Approved :: GNU General Public License (GPL)",
+        classifiers=[
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: System Administrators",
+            # "Intended Audience :: Developers",
+            # TODO: For when I finish the API rework.
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Operating System :: POSIX",
+            #"Operating System :: OS Independent",
+            # TODO: For when the stdlib-based zip/tar support is ready.
+            "Programming Language :: Python",
+            #TODO: Add support for Python 3 and an appropriate classifier
+            "Topic :: System :: Archiving",
+            "Topic :: Utilities",
+        ],
+
+        packages=['unball'],
+        scripts=['src/moveToZip'],
+        zip_safe=True,
+
+        #TODO: Forget setuptools. Just replace this with a stub script.
+        entry_points={
+            'console_scripts': [
+                'unball = unball.main:main_func',
+            ],
+        },
+        #data_files=[
+        #    ('share/man/man1', ['build/man/unball.1']),
+        #    ('share/apps/konqueror/servicemenus', [
+        #        'src/servicemenus/unball.desktop',
+        #        'src/servicemenus/moveToZip.desktop'
+        #    ]),
+        #    ('libexec/thunar-archive-plugin', ['src/unball.tap'])
+        #],
+
+        #cmdclass={'build_manpage': _build_manpage},
+        test_suite='run_test.get_tests',
+
+        options={
+            'build_manpage': {
+                'output': 'build/man/unball.1',
+                'parser': 'unball:get_opt_parser',
+            },
+        },
+    )
